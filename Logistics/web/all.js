e8810454193cb858fa944f2fe3e59647a1fa36df
wangxiaolong <wangxiaolong815@163.com>
2017-07-20 09:59:02 +0800

    新的物流界面

---
+++ Logistics/web/all.js
@@ -0,0 +1,3 @@
+document.write("<scri" + "pt src='../js/zepto.min.js'></sc" + "ript>");
+document.write("<scri" + "pt src='../js/sm.min.js'></sc" + "ript>");
+document.write("<scri" + "pt src='../js/sm-extend.min.js'></sc" + "ript>");
